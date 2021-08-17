9361403a523cdbe188abb12ed542c8b2e42e06bc
DESKTOP-O9AU6CA\rollingstone <aRollingStone27@gmail.com>
2021-08-17 19:41:18 +0900

    screenshot 프로그램

--- GUIProject/2. project/5_apply_options.py
+++ GUIProject/2. project/5_apply_options.py
@@ -28,6 +28,7 @@ def browseDestPath():
     folder_selected = filedialog.askdirectory()
 
     if folder_selected == "":
+        print("폴더 선택 취소")
         return
     
     txt_dest_path.delete(0, END)
@@ -35,86 +36,91 @@ def browseDestPath():
 
 # 이미지 통합
 def merge_image():
+
+    try:
+
        
-    # 가로 넓이
-    img_width = cmb_width.get()
-    if img_width == "원본유지":
-        img_width = -1
-    else:
-        img_width = int(img_width)
+        # 가로 넓이
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1
+        else:
+            img_width = int(img_width)
 
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == "좁게":
-        img_space = 30
-    elif img_space == "보통":
-        img_space = 60
-    elif img_space == "넓게":
-        img_space = 90
-    else:
-        img_space = 0
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_space = 90
+        else:
+            img_space = 0
 
-    # 포맷
-    img_format = cmb_format.get().lower() # PNG, JPG, BMP 등 값을 소문자로 변경
+        # 포맷
+        img_format = cmb_format.get().lower() # PNG, JPG, BMP 등 값을 소문자로 변경
 
  
 
-    # print(list_file.get(0, END)) # 모든 파일 목록 출력
-    images = [Image.open(x) for x in list_file.get(0, END)]
+        # print(list_file.get(0, END)) # 모든 파일 목록 출력
+        images = [Image.open(x) for x in list_file.get(0, END)]
 
 
-    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_size = [] # [(width1, height1), (width2, height2), ....]
-    if img_width > -1:
-        image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
-    else:
-        # 원본 사이즈 사용
-        image_sizes = [(x.size[0], x.size[1]) for x in images]
-
-    # 계산식
-    # 100 * 60  이미지가 있음 -> width를 80으로 줄이면 height는?
-    # (원본 width) : (원본 height) = (변경 width) : (변경 height)
-    #     100      : 60            =    80      : ?
-    #      x       :   y           =     x'     :   y'
-    #      xy' = x'y
-    #        y' = x'y / x ->  이 식을 적용
-    # size -> size[0] : witdh, size[1] : height
-#    widths = [x.size[0] for x in images]
- #   heights = [x.size[1] for x in images]
-    widths, heights = zip(*(image_sizes))
-
-    # 최대 넓이, 전체 높이 구해옴
-    max_width, total_height = max(widths), sum(heights)
-    
-    # 스케치북 준비
-    if img_space > 0:
-        total_height += (img_space * (len(images) -1))
-
-    result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255))
-    y_offset = 0 # y 위치 정보
-
-
-    # for img in images:
-      #  result_img.paste(img, (0, y_offset))
-      # y_offset += img.size[1]
-    
-    for idx, img in enumerate(images):
-        # width가 원본유지가 아닐 때에는 이미지 크기 조정
+        # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_size = [] # [(width1, height1), (width2, height2), ....]
         if img_width > -1:
-            img = img.resize(image_sizes[idx])
+            image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
+        else:
+            # 원본 사이즈 사용
+            image_sizes = [(x.size[0], x.size[1]) for x in images]
 
-        result_img.paste(img, (0, y_offset))
-        y_offset += (img.size[1] + img_space)
+        # 계산식
+        # 100 * 60  이미지가 있음 -> width를 80으로 줄이면 height는?
+        # (원본 width) : (원본 height) = (변경 width) : (변경 height)
+        #     100      : 60            =    80      : ?
+        #      x       :   y           =     x'     :   y'
+        #      xy' = x'y
+        #        y' = x'y / x ->  이 식을 적용
+        # size -> size[0] : witdh, size[1] : height
+    #    widths = [x.size[0] for x in images]
+     #   heights = [x.size[1] for x in images]
+        widths, heights = zip(*(image_sizes))
 
-        progress = (idx + 1) / len(images) * 100
-        p_var.set(progress)
-        progress_bar.update()
+        # 최대 넓이, 전체 높이 구해옴
+        max_width, total_height = max(widths), sum(heights)
     
-    # 포맷 옵션 처리
-    file_name = "merget_photo." + img_format
-    dest_path = os.path.join(txt_dest_path.get(), file_name)
-    result_img.save(dest_path)
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
+        # 스케치북 준비
+        if img_space > 0:
+            total_height += (img_space * (len(images) -1))
+
+        result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255))
+        y_offset = 0 # y 위치 정보
+
+
+        # for img in images:
+          #  result_img.paste(img, (0, y_offset))
+          # y_offset += img.size[1]
+    
+        for idx, img in enumerate(images):
+            # width가 원본유지가 아닐 때에는 이미지 크기 조정
+            if img_width > -1:
+                img = img.resize(image_sizes[idx])
+
+            result_img.paste(img, (0, y_offset))
+            y_offset += (img.size[1] + img_space)
+
+            progress = (idx + 1) / len(images) * 100
+            p_var.set(progress)
+            progress_bar.update()
+    
+        # 포맷 옵션 처리
+        file_name = "merget_photo." + img_format
+        dest_path = os.path.join(txt_dest_path.get(), file_name)
+        result_img.save(dest_path)
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+    except Exception as err:
+        msgbox.showerror("에러", err)
 
 
 # 시작
