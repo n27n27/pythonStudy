7c8ba680fd5fac1e83c47cb4f87790cb6ee347e5
Rolling Stone <aRollingStone27@gmail.com>
2021-09-01 17:08:19 +0900

    headless 쓰려면 user agent 변경이 필요할 때가 있음

---
+++ webScraping/1. basic/18_headless_chrome_useragent.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import time
+
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("window-size=1920x1080")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36")
+
+browser = webdriver.Chrome("C:/Users/n27/Desktop/개발공부/pythonStudy/chromedriver.exe", options=options)
+browser.maximize_window()
+
+url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
+browser.get(url)
+detected_value = browser.find_element_by_id("detected_value")
+print(detected_value.text)
+browser.quit()
