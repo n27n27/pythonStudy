ffa93e3cb28c7a7e9272332c0e9bba58777c0d2c
Rolling Stone <aRollingStone27@gmail.com>
2021-09-01 16:25:25 +0900

    동적페이지데이터 가져오기 위한 스크롤 내리기

--- webScraping/1. basic/16_selenium_movies_scroll.py
+++ webScraping/1. basic/16_selenium_movies_scroll.py
@@ -1,10 +1,37 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
+import time
+
+browser = webdriver.Chrome("C:/Users/n27/Desktop/개발공부/pythonStudy/chromedriver.exe")
 browser.maximize_window()
 
 # 페이지 이동
 url = "https://play.google.com/store/movies/top"
 browser.get(url)
 
-# 스크롤 내리기
-browser.execute_script("windows.scrollTo(0, 1080") # 해상도 1920 x 1080
+# 지정한 위치로 스크롤 내리기
+# browser.execute_script("windows.scrollTo(0, 1080)") # 해상도 1920 x 1080
+
+# 화면 가장 아래로 스크롤 내리기
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+interval = 2 
+
+# 현재 문서에 높이를 가져와서 저장
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+# 반복 수행
+while True:
+    # 스크롤을 가장 아래로 내림
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    time.sleep(interval)
+
+    # 현재 문서 높이를 가져와서 저장
+    curr_height = browser.execute_script("return document.body.scrollHeight")
+    if curr_height == prev_height:
+        break
+
+    prev_height = curr_height
+
+
+print("스크롤 완료")
